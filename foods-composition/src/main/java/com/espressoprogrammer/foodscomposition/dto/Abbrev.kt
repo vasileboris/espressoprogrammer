dc77264a176bca2160f2258d84c093d87307c1b8
vasile boris <boris@espressoprogrammer.com>
2016-08-03 08:44:19 +0300

    Add foods-composition project

---
+++ foods-composition/src/main/java/com/espressoprogrammer/foodscomposition/dto/Abbrev.kt
@@ -0,0 +1,9 @@
+package com.espressoprogrammer.foodscomposition.dto
+
+/**
+ * It contains values from ABBREV.txt file
+ */
+data class Abbrev(val ndbNo: String,
+                  val shrtDesc: String,
+                  val energKcal: Int) {
+}
